022df8a8ee8309a81a8c36e02d48e417b9d70985
Nico Opiyo <v-niopiy@microsoft.com>
2020-02-28 01:04:48 +0300

    Added subtraction changes

--- MathConsoleApp/MathConsoleApp/Program.cs
+++ MathConsoleApp/MathConsoleApp/Program.cs
@@ -7,9 +7,14 @@ namespace MathConsoleApp
     {
         static void Main(string[] args)
         {
-            MathFunctions addFunction = new MathFunctions();
-            int value = addFunction.sumValues(10, 20);
-            Console.WriteLine(value);
+            MathFunctions mathFunctions = new MathFunctions();
+            int sum = mathFunctions.sumValues(10, 20);
+            Console.WriteLine(sum);
+
+            int difference = mathFunctions.subtract(20, 10);
+            Console.WriteLine(difference);
+
+            Console.ReadKey();
         }
     }
 }
