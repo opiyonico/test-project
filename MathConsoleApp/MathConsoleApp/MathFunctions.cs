7281969a33f211450de9e4ffcc5229b1bd4cf723
samson nyore <samsonnyore@gmail.com>
2020-02-27 22:53:44 +0300

    console app initial commit

---
+++ MathConsoleApp/MathConsoleApp/MathFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathConsoleApp
+{
+    public class MathFunctions
+    {
+        public int sumValues(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+    }
+}
